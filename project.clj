b9f36e525d8e083cd1dd41ad269d9972e4ef273b
Tero Paloheimo <tero.paloheimo@iki.fi>
2014-10-23 20:01:02 +0300

    Added necessary prerequisites for ring and compojure

--- project.clj
+++ project.clj
@@ -1,12 +1,19 @@
 (defproject hstop1k "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A search and display application for Q-Dance Harder Styles Top
+               1000 songs"
   :url "https://github.com/terop/env-logger"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [enlive "1.1.5"]
                  [korma "0.4.0"]
-                 [org.xerial/sqlite-jdbc "3.8.6"]]
-  :main ^:skip-aot hstop1k.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+                 [org.xerial/sqlite-jdbc "3.8.6"]
+                 [compojure "1.2.0"]
+                 [ring/ring-defaults "0.1.2"]
+                 [de.ubercode.clostache/clostache "1.4.0"]]
+  :plugins [[lein-ring "0.8.13"]]
+  :ring {:handler hstop1k.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring-mock "0.1.5"]]}})
