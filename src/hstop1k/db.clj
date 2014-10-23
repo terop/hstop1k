fe6c4b612bb039def5fa0b5c41ab20739054d473
Tero Paloheimo <tero.paloheimo@iki.fi>
2014-10-23 20:45:52 +0300

    Added an index template with a table of all songs

--- src/hstop1k/db.clj
+++ src/hstop1k/db.clj
@@ -1,7 +1,6 @@
 (ns hstop1k.db
   (:require [korma.core :as kc]
-            [korma.db :as kd])
-  (:gen-class))
+            [korma.db :as kd]))
 
 ;; The korma DB reference
 (kd/defdb db
@@ -16,6 +15,12 @@
   "Inserts one row to the songs table"
   [place artist song]
   (kc/insert songs
-             (kc/values {:place place
-                         :artist artist
-                         :song song})))
+    (kc/values {:place place
+                :artist artist
+                :song song})))
+
+(defn select-songs
+  "Returns all the songs in the DB as a vector of maps"
+  []
+  (vec (kc/select songs
+         (kc/order :place :ASC))))
--- src/hstop1k/handler.clj
+++ src/hstop1k/handler.clj
@@ -1,10 +1,25 @@
 (ns hstop1k.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [clostache.parser :refer [render]]
+            [hstop1k.db :as db]))
+
+;; Template Rendering
+(defn read-template
+  "Reads a template with the provided name."
+  [template-name]
+  (slurp (clojure.java.io/resource (str "templates/" template-name
+                                        ".mustache"))))
+
+(defn render-template
+  "Renders the template name with the provided parameters."
+  [template-file params]
+  (render (read-template template-file) params))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (render-template "index"
+                               {:songs (db/select-songs)}))
   (route/not-found "Not Found"))
 
 (def app
