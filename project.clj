1b9588c83f4305eda092d1da4de97f7c3b1660e9
Tero Paloheimo <tero.paloheimo@iki.fi>
2018-10-05 21:24:28 +0300

    Update dependencies

--- project.clj
+++ project.clj
@@ -5,15 +5,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [enlive "1.1.6"]
-                 [korma "0.4.2"]
-                 [org.xerial/sqlite-jdbc "3.8.11.2"]
-                 [compojure "1.4.0"]
-                 [ring/ring-defaults "0.1.5"]
-                 [selmer "0.9.5"]]
+                 [korma "0.4.3"]
+                 [org.xerial/sqlite-jdbc "3.25.2"]
+                 [compojure "1.6.1"]
+                 [ring/ring-defaults "0.3.2"]
+                 [selmer "1.12.1"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler hstop1k.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]}})
+                        [ring/ring-mock "0.3.2"]]}})
